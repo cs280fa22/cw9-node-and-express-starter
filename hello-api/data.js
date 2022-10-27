90d68c3935ba1e4c77a55507fdebb9df8dd3325c
Ali Madooei <alimadooei@gmail.com>
2022-10-27 11:36:16 -0400

    Initial Commit

---
+++ hello-api/data.js
@@ -0,0 +1,102 @@
+export const schools = [
+  {
+    Name: "Whiting School of Engineering",
+  },
+];
+
+export const terms = [
+  {
+    Name: "Fall 2019",
+  },
+  {
+    Name: "Spring 2020",
+  },
+  {
+    Name: "Fall 2020",
+  },
+  {
+    Name: "Spring 2021",
+  },
+  {
+    Name: "Fall 2021",
+  },
+  {
+    Name: "Spring 2022",
+  },
+  {
+    Name: "Fall 2022",
+  },
+];
+
+export const courses = [
+  {
+    OfferingName: "EN.500.112",
+    Title: "Gateway Computing: JAVA",
+    Term: "Fall 2019",
+  },
+  {
+    OfferingName: "EN.601.226",
+    Title: "Data Structures",
+    Term: "Fall 2019",
+  },
+  {
+    OfferingName: "EN.601.226",
+    Title: "Data Structures",
+    Term: "Spring 2020",
+  },
+  {
+    OfferingName: "EN.601.421",
+    Title: "Object Oriented Software Engineering",
+    Term: "Spring 2020",
+  },
+  {
+    OfferingName: "EN.601.226",
+    Title: "Data Structures",
+    Term: "Fall 2020",
+  },
+  {
+    OfferingName: "EN.601.280",
+    Title: "Full-Stack JavaScript",
+    Term: "Fall 2020",
+  },
+  {
+    OfferingName: "EN.601.226",
+    Title: "Data Structures",
+    Term: "Spring 2021",
+  },
+  {
+    OfferingName: "EN.601.421",
+    Title: "Object Oriented Software Engineering",
+    Term: "Spring 2021",
+  },
+  {
+    OfferingName: "EN.601.226",
+    Title: "Data Structures",
+    Term: "Fall 2021",
+  },
+  {
+    OfferingName: "EN.601.280",
+    Title: "Full-Stack JavaScript",
+    Term: "Fall 2021",
+  },
+  {
+    OfferingName: "EN.601.226",
+    Title: "Data Structures",
+    Term: "Spring 2022",
+  },
+  {
+    OfferingName: "EN.601.421",
+    Title: "Object Oriented Software Engineering",
+    Term: "Spring 2022",
+  },
+  {
+    OfferingName: "EN.601.226",
+    Title: "Data Structures",
+    Term: "Fall 2022",
+  },
+  {
+    OfferingName: "EN.601.280",
+    Title: "Full-Stack JavaScript",
+    Term: "Fall 2022",
+  },
+];
